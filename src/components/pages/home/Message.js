1cbe53ed0f227ef22095e4e1c0aa53da9e419e1e
CarlosHenrique7777 <carloshenrique.webdev@gmail.com>
2024-06-21 23:28:10 -0300

    feat: Implement Message section with animations

--- src/components/pages/home/Message.js
+++ src/components/pages/home/Message.js
@@ -1,13 +1,66 @@
 import { useEffect, useRef, useState } from "react";
 import DetailText from "@/components/shared/DetailText";
+import ImageAnimation from "@/components/shared/ImageAnimation";
 import { useTranslation } from "next-i18next";
+import Sparkle from 'react-sparkle'
 
 export default function Message({ translations }) {
   const { t } = useTranslation();
-  
-  return (
-    <div className="px-[24px] py-[24px] lg:px-[80px] mx-auto max-w-[640px] md:max-w-full xl:max-w-[1280px] flex flex-col gap-[32px]">
 
+    const targetRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); // Stop observing after the animation is triggered
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => {
+      if (targetRef.current) {
+        observer.unobserve(targetRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={targetRef} className={`py-[100px] bg-cover bg-fixed bg-[url('/assets/images/img3.jpg')] border-solid border-y-[8px] border-[#020201] ${isVisible ? 'bg-pan-br' : ''}`}>
+      <div className="bg-deep-night py-[56px]">
+        <div className="px-[24px] lg:px-[80px] mx-auto max-w-[640px] md:max-w-full xl:max-w-[1280px] flex flex-col gap-[24px] md:justify-center md:items-center md:text-center">
+          <div className="relative w-fit">
+            <img
+              src="/assets/images/she1.jpg"
+              alt=""
+              className="block border-solid border-[1px] border-golden-sunbeam rounded-[100%] object-cover h-[50px] w-[50px]"
+            />
+            <Sparkle count={7} color={'#E7E3E7'} fadeOutSpeed={15} />
+          </div>
+
+          <div className="flex flex-col gap-[20px] md:justify-center md:items-center">
+            <div className="flex flex-col gap-[16px] md:justify-center md:items-center">
+              <DetailText text={t("introductionSubtitle")} />
+              <h2 className="uppercase text-[24px] font-bold">
+                {t("introductionTitle")}
+              </h2>
+            </div>
+
+            <div className="flex flex-col gap-[16px] text-light-gray font-medium">
+              <p className="md:max-w-[800px]">{t("whyUsDescription")}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
