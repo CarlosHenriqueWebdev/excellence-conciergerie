d4f305d50721b80d41eaff41a3508e0b4d8aec99
CarlosHenrique7777 <carloshenrique.webdev@gmail.com>
2024-10-19 12:19:49 -0300

    feat: added google tag manager script and iframe

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,23 +5,33 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/faviconIcon.ico" />
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3DGMK3CQ1Q"
-        ></script>
+
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-3DGMK3CQ1Q');
-            `,
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K3NN6SNG');
+          `,
           }}
         />
+        {/* End Google Tag Manager */}
       </Head>
       <body className="text-lavender-haze bg-midnight-blue">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K3NN6SNG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Main />
         <NextScript />
       </body>
